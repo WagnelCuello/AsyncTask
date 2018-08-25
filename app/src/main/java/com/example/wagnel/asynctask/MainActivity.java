55daf907116a73d8545caf0d981a806ad91d927e
Wagnel <wagnelcuello@gmail.com>
2018-08-25 19:45:40 +0200

    second commit

--- app/src/main/java/com/example/wagnel/asynctask/MainActivity.java
+++ app/src/main/java/com/example/wagnel/asynctask/MainActivity.java
@@ -9,5 +9,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout, new BlankFragment())
+                .addToBackStack("Blue")
+                .commit();
+                */
     }
 }
